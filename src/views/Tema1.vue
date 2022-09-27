0bc9a114bc1f7154ae1718e124ea085ee1b6a7d3
elrilacho <camilobr17@gmail.com>
2022-09-27 09:39:21 -0500

    Tema 1 avances

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -49,11 +49,181 @@
     .row.justify-content-center.mb-5
       .col-lg-5
         .tarjetas-icono-up
-          .tarjetas-icono-up__icono(style='background-color: #F0BD51').p-3
-            img(src='@/assets/curso/temas/tema1/3.svg', alt='')
-          .tarjetas-icono-up__contenido(style='background-color: #FDF5E5').p-4
-            h5.mb-0 Planificación
-            p.mb-0 Es la etapa en la que se establecen los objetivos a alcanzar, se definen las estrategias y se establecen los recursos necesarios para alcanzar los objetivos.
+          .tarjetas-icono-up__icono(style='background-color: #F0BD51')
+            .icono
+              img(src='@/assets/curso/temas/tema1/3.svg', alt='')
+          .tarjetas-icono-up__contenido(style='background-color: #FDF5E5').p-4.h-100
+            h5.mb-0 Fase mecánica
+            p.mb-0 Es la parte estática del proceso y está conformada por las etapas de planificación y organización. 
+      .col-lg-5
+        .tarjetas-icono-up
+          .tarjetas-icono-up__icono(style='background-color: #EF8B53')
+            .icono
+              img(src='@/assets/curso/temas/tema1/4.svg', alt='')
+          .tarjetas-icono-up__contenido(style='background-color: #F7E9E1').p-4.h-100
+            h5.mb-0 Fase dinámica
+            p.mb-0 Esta hace referencia al movimiento y está conformada por la dirección y el control.
+    p.mb-5(data-aos='fade-right') Amplíe la información sobre cada una de estas etapas:
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .imagen-info-mod
+          ImagenInfografica.color-acento-botones.mb-5
+            template(v-slot:imagen)
+              figure
+                img(src='@/assets/curso/temas/tema1/5.svg', alt='Texto que describa la imagen')
+            .tarjeta.rounded-0.p-3(x="19%" y="40%" numero="" style='background-color: #F6F6F6')
+              img(src='@/assets/curso/temas/tema1/6.svg', alt='' style='width: 82px')
+              h5 03. Dirección
+              p Consiste en que la organización verifique que se hagan las actividades o tareas propuestas para el logro de los objetivos, mediante el liderazgo, la motivación y la comunicación de los involucrados.
+            .tarjeta.rounded-0.p-3(x="50%" y="4%" numero="" style='background-color: #F6F6F6')
+              img(src='@/assets/curso/temas/tema1/7.svg', alt='' style='width: 82px')
+              h5 04. Control
+              p En esta última etapa, la organización mide los resultados de cómo se hicieron las actividades planteadas y efectuadas, con el objetivo de generar mejoras continuas.
+            .tarjeta.rounded-0.p-3(x="82%" y="48%" numero="" style='background-color: #F6F6F6')
+              img(src='@/assets/curso/temas/tema1/8.svg', alt='' style='width: 82px')
+              h5 01. Planficación
+              p Esta etapa consiste en que la organización determine qué se va a hacer, de forma anticipada, para lograr los objetivos propuestos de manera eficiente.
+            .tarjeta.rounded-0.p-3(x="50%" y="96%" numero="" style='background-color: #F6F6F6')
+              img(src='@/assets/curso/temas/tema1/9.svg', alt='' style='width: 82px')
+              h5 02. Organización
+              p En esta etapa, la organización identifica cómo hacer que los recursos se utilicen de forma equilibrada, manteniendo una relación entre el trabajo y el personal que lo va a realizar o ejecutar.
+
+
+
+
+    //TODO agregar el enlace ---------------------------------------------------------
+    .tarjeta.color-primario.p-4.mb-5
+      .row.justify-content-around.align-items-center
+        .col-3.col-sm-2.col-lg-1
+          img(src="@/assets/curso/temas/tema1/10.svg")
+        .col
+          .row.justify-content-between.align-items-center
+            .col.mb-3.mb-sm-0
+              h3.mb-1 Qué es el Proceso Administrativo y cuáles son sus etapas?
+              p.text-small.mb-0 Para complementar este tema, observe el video: ¿Qué es el Proceso Administrativo y cuáles son sus etapas?, que se encuentra en el material complementario de este componente formativo.
+            .col-sm-auto
+              a.boton.color-acento-botones(:href="obtenerLink('downloads/prueba.pdf')" target="_blank")
+                span Ir al enlace
+                i.fas.fa-link
+
+    .titulo(style='background-color: #F0BD51').d-inline-block.p-3.mb-4
+      h4.mb-0 La planificación en la organización
+    p.mb-5(data-aos='fade-right') Es el proceso en el que una empresa u organización planifica sus actividades diarias. Disponer de este mecanismo les permite a las organizaciones alcanzar los objetivos establecidos, de una manera estructurada, eficaz y centrada en la obtención de resultados. Existen cuatro tipos de planificación organizacional:
+    TabsB.color-acento-contenido.mb-5
+      .py-4.py-md-5(titulo="Planificación organizacional estratégica" :icono="require('@/assets/curso/temas/tema1/11.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0
+            h4 Planificación organizacional estratégica
+            p Consiste en desarrollar un plan de atención general para todas las áreas de una empresa. La formulación de objetivos se realiza basándose en un periodo de tiempo, ya sea a corto, mediano o largo plazo; procurando que dichos objetivos estén adaptados con los valores empresariales, además de visión y misión. 
+              br
+              br
+              | Generalmente, este tipo de planificación organizacional es hecha por altos ejecutivos de una empresa, ya que participan todas las áreas de una organización y las decisiones deben ser tomadas de forma estratégica.  Aunque hay pequeñas empresas que prefieren integrar a todos sus empleados, con el fin de aportar valor a la formulación de objetivos.
+        
+          .col-md-4
+            figure
+              img(src='@/assets/curso/temas/tema1/12.jpg', alt='Texto que describa la imagen' style='border-radius: 20px ')
+      .py-4.py-md-5(titulo="Planificación organizacional táctica" :icono="require('@/assets/curso/temas/tema1/11.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0
+            h4 Planificación organizacional táctica
+            p Implica describir la manera en que una compañía va a aplicar su plan estratégico. Sin embargo, este es más específico y se establece por algunos objetivos a corto plazo que apoyan el plan estratégico; su duración de acción y ejecución es de aproximadamente un año o menos.
+              br
+              br
+              |El plan táctico suele ser orientado por los gerentes de cada área, que dan supervisión a aquellos objetivos a corto plazo y deben aplicar acciones, estrategias y delegar funciones para dar cumplimiento a las metas establecidas.
+        
+          .col-md-4
+            figure
+              img(src='@/assets/curso/temas/tema1/13.jpg', alt='Texto que describa la imagen' style='border-radius: 20px ')
+      .py-4.py-md-5(titulo="Planificación organizacional operacional" :icono="require('@/assets/curso/temas/tema1/11.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0
+            h4 Planificación organizacional operacional
+            p Es la visión y el control de los aspectos diarios en la planificación organizacional. Está conformada por labores que se realizan a diario, como actividades de operación, normativa de trabajo, horarios, cumplimiento de políticas y estándares empresariales. 
+              br
+              br
+              |Asimismo, permite asegurar que las responsabilidades, funciones y tareas se lleven a cabo de manera eficaz y los encargados de estas procedan de acuerdo con lo establecido por la empresa.
+        
+          .col-md-4
+            figure
+              img(src='@/assets/curso/temas/tema1/14.jpg', alt='Texto que describa la imagen' style='border-radius: 20px ')
+      .py-4.py-md-5(titulo="Planificación organizacional de contingencia" :icono="require('@/assets/curso/temas/tema1/11.svg')")
+        .row.justify-content-center
+          .col-md-6.mb-4.mb-md-0
+            h4 Planificación organizacional de contingencia
+            p El plan de contingencia se utiliza de forma esporádica, en especial, cuando surgen imprevistos, crisis o escenarios inusuales. Es un plan estructurado y funcional, que comprende una serie de retos o desafíos, el cual se debe abordar si una situación inesperada ocurre. Aunque las empresas consideren que es un plan que nunca van a utilizar, es esencial contar con este. 
+        
+          .col-md-4
+            figure
+              img(src='@/assets/curso/temas/tema1/15.jpg', alt='Texto que describa la imagen' style='border-radius: 20px ')
+    .titulo(style='background-color: #F0BD51').d-inline-block.p-3.mb-4
+      h4.mb-0 Características y elementos de la organización
+    p.mb-5(data-aos='fade-right') Una organización se define como un conjunto de individuos que cooperan entre sí para alcanzar determinados objetivos y metas. Según Max Weber (1922), una organización es: #[b “un grupo de personas que se asocian con el propósito de lograr un objetivo y que está direccionado a través de reglas y órdenes que se dan mediante un jefe”.] A continuación, conozca las características y elementos de la organización:
+    .margin-n(style='background-color: #FFF9ED').p-5
+      .row.justify-content-center.mb-5
+        .col-lg-9
+          .row.justify-content-center.mb-2.g-0
+            .col-lg-2
+              div(style='background-color: #F0BD51').p-4.h-100.d-flex.align-items-center
+                img(src='@/assets/curso/temas/tema1/16.svg', alt='' style='width: 59px').m-auto
+            .col-lg-10
+              div(style='background-color: #FAEBCD').p-5.h-100
+                p.mb-0 #[b Es un sistema social.] Está conformada por individuos, donde se asignan unos objetivos alineados por métodos y ocupaciones que causan una permanencia en equilibrio y comunicación. Estos componentes se relacionan entre sí, de tal manera que algún cambio o transformación en ellos puede provocar alteraciones en los resultados.  
+          .row.justify-content-center.mb-2.g-0
+            .col-lg-2
+              div(style='background-color: #F0BD51 ').p-4.h-100.d-flex.align-items-center
+                img(src='@/assets/curso/temas/tema1/17.svg', alt='' style='width: 59px').m-auto
+            .col-lg-10
+              div(style='background-color: #FAEBCD').p-5.h-100
+                p.mb-0 #[b Se mantienen en el tiempo.] Las organizaciones tienen la posibilidad de proyectarse o no por un tiempo mayor que la vida de quienes fueron sus fundadores.
+          .row.justify-content-center.mb-2.g-0
+            .col-lg-2
+              div(style='background-color: #F0BD51 ').p-4.h-100.d-flex.align-items-center
+                img(src='@/assets/curso/temas/tema1/18.svg', alt='' style='width: 59px').m-auto
+            .col-lg-10
+              div(style='background-color: #FAEBCD').p-5.h-100.d-flex.align-items-center
+                p.mb-0 #[b Son complejas.] El entorno constantemente experimenta cambios, lo que hace que las organizaciones originen nuevas formas de operar de forma innovadora. Cada organización definirá su programa de trabajo con respecto a las diferentes áreas funcionales.
+          .row.justify-content-center.mb-2.g-0
+            .col-lg-2
+              div(style='background-color: #F0BD51 ').p-4.h-100.d-flex.align-items-center
+                img(src='@/assets/curso/temas/tema1/16.svg', alt='' style='width: 59px').m-auto
+            .col-lg-10
+              div(style='background-color: #FAEBCD').p-5.h-100
+                p.mb-0 #[b Son dinámicas.] Deben adaptarse a los cambios que acompañan la evolución de la humanidad, desde su inicio hasta la actualidad. Su esquema, por ende, es relativamente estable.
+          .row.justify-content-center.mb-2.g-0
+            .col-lg-2
+              div(style='background-color: #F0BD51 ').p-4.h-100.d-flex.align-items-center
+                img(src='@/assets/curso/temas/tema1/19.svg', alt='' style='width: 59px').m-auto
+            .col-lg-10
+              div(style='background-color: #FAEBCD').p-5.h-100
+                p.mb-0 #[b Dividen el trabajo.] Todas las áreas deberán trabajar en equipo y colaborarse entre sí con el fin de actuar cooperativamente. Esto genera mejores rendimientos y mayores beneficios.
+          .row.justify-content-center.mb-2.g-0
+            .col-lg-2
+              div(style='background-color: #F0BD51 ').p-4.h-100.d-flex.align-items-center
+                img(src='@/assets/curso/temas/tema1/20.svg', alt='' style='width: 59px').m-auto
+            .col-lg-10
+              div(style='background-color: #FAEBCD').p-5.h-100
+                p.mb-0 #[b Son innovadoras.] Las organizaciones deben ser creativas, con el propósito de crear nuevos procesos en el medio en que se desenvuelven, influenciado por los efectos de la globalización. De hecho, los recursos humanos van integrando condiciones de su entorno, se van identificando con esa organización. Adaptan su conducta, impulsando y optimizando las operaciones de la organización.
+          .row.justify-content-center.mb-2.g-0
+            .col-lg-1
+              div(style='background-color: #F0BD51 ').p-4.h-100.d-flex.align-items-center
+                img(src='@/assets/curso/temas/tema1/21.svg', alt='' style='width: 59px').m-auto
+            .col-lg-10
+              div(style='background-color: #FAEBCD').p-5.h-100
+                p.mb-0 #[b Persiguen la eficiencia.] Todos los recursos deben ser eficientes. Es decir, obtienen un máximo rendimiento, en relación con calidad y cantidad, con un menor costo o gasto de recursos. 
+          
+
+    
+
+              
+
+
+
+
+
+
+
+
+
 
 </template>
 
@@ -75,4 +245,19 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.tarjetas-icono-up__icono
+  position: relative
+  padding: 5rem
+  height: 100%
+  width: 100%
+  .icono
+    position: absolute
+    width: 100%
+    left: 0
+    top: 0
+    transform: translateY(-20%)
+    img
+      width: 300px
+      margin: 0 auto
+</style>
